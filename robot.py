9ab975a283cc99f69c2d24dda079b7a078d4360b
julianVelandia <jvelandiag@unal.edu.co>
2022-10-25 19:50:38 -0500

    First aproach robot joins

--- robot.py
+++ robot.py
@@ -1,27 +1,23 @@
-'''
 import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import JointState
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 import termios, sys, os
-from numpy import pi
-
-TERMIOS = termios
-'''
 import math
 
+TERMIOS = termios
 
 class PhantomX:
 
     def joint_publisher(self, points: [[]], is_degree=None):
-        '''
+
         pub = rospy.Publisher('/joint_trajectory', JointTrajectory, queue_size=0)
         rospy.init_node('joint_publisher', anonymous=False)
         state = JointTrajectory()
         state.header.stamp = rospy.Time.now()
         state.joint_names = ["joint_1", "joint_2", "joint_3", "joint_4", "joint_5"]
         point = JointTrajectoryPoint()
-        '''
+
         for point in points:
             if point[0] == '':
                 if menu_while() == 'x':
@@ -29,17 +25,15 @@ class PhantomX:
                 else:
                     continue
 
-            if is_degree is None:
-                print(point)
-            else:
-                print(degrees_to_radians(point))
-            '''
+            if is_degree is not None:
+                point = degrees_to_radians(point)
+
             point.positions = point
             point.time_from_start = rospy.Duration(0.5)
             state.points.append(point)
             pub.publish(state)
             rospy.sleep(1)
-            '''
+
 
 
 def menu_while():
