9e6f4e22e44a191a7d1153fa751d274800802130
SebastianCubides <scubidest@unal.edu.co>
2022-11-01 21:39:15 -0500

    Testing

--- get_points.py
+++ get_points.py
@@ -17,29 +17,35 @@ def read_csv_XY():
     file_name = 'csv/test.csv'
 
     response = []
+    Q = []
     with open(file_name, newline='') as File:
         reader = csv.reader(File)
         for row in reader:
-            XYZ.append(row)
-    for cor in XYZ:
-    	x = float(cor[0])
-    	y = float(cor[1])
-    	z = float(cor[2])
-    	L1 = 4
-    	L2 = 10.5
-    	L3 = 10.5
-    	L4 = 9.1
-    	d = 8
-    	r = math.sqrt(x**2+z**2)
-    	a = math.sqrt((L1+d)**2+r**2)-L4
-    	
-    	q1 = math.atan(y/x)        
-    	q2 = math.acos(a/(2*L2))
-    	q3 = -math.acos(((a**2)/(2*L2**2))-1)
-    	q4 = q2
-    	q5 = 1.5
-    	
-    	response.append([q1,q2,q3,q4,q5])
-    
-    return response
+            response.append(row)
+    for cor in response:
+        x = float(cor[0])
+        y = float(cor[1])
+        z = float(cor[2])
+        L1 = 4
+        L2 = 10.5
+        L3 = 10.5
+        L4 = 9.1
+        d = 8
 
+        D = math.sqrt(x**2+y**2)
+        beta = math.atan((z-L1)/D)
+        r = math.sqrt(D**2-z**2)
+        cosAlfa = (r-L4)/(2*L2)
+        sinAlfa = math.sqrt(1-cosAlfa**2)
+        Alfa = math.atan2(sinAlfa,cosAlfa)
+        q_1 = math.atan2(y,x)
+        q_2 = -(Alfa+beta)
+        cosQ3 = ((r-L4)**2/(2*L2**2))-1
+        sinQ3 = math.sqrt(1-cosQ3**2)
+        q_3 = -math.atan2(sinQ3,cosQ3)
+        q_4 = Alfa
+        q_5 = math.pi/2
+    	
+        Q.append([q_1,q_2,q_3,q_4,q_5])
+    
+    return Q
--- robot.py
+++ robot.py
@@ -17,13 +17,8 @@ class PhantomX:
         state.header.stamp = rospy.Time.now()
         state.joint_names = ["joint_1", "joint_2", "joint_3", "joint_4", "joint_5"]
         point = JointTrajectoryPoint()
-
+        
         for pointArray in points:
-            if pointArray[0] == '':
-                if menu_while() == 'x':
-                    break
-                else:
-                    continue
 
             if is_degree is not None:
                 pointArray = degrees_to_radians(pointArray)
@@ -34,18 +29,6 @@ class PhantomX:
             state.points.append(point)
             pub.publish(state)
             rospy.sleep(1)
-
-
-
-def menu_while():
-    while True:
-        key = input('\nSalto de linea '
-                    '\n\n  - Pulse x para finalizar '
-                    '\n  -Pulse c para continuar\n')
-        if key.lower() == 'x' or key.lower() == 'c':
-            break
-    return key
-
 
 def degrees_to_radians(degrees: []):
     return list(map(lambda d: (float(d) * math.pi / 180), degrees))
